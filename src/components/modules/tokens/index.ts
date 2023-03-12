8cedeca10aa6ebd8c1bf4e47f42f5de670e7021e
Alexandru Berce <alex@devaccent.com>
2023-03-12 16:23:25 +0200

    Added new section to Dashboard

---
+++ src/components/modules/tokens/index.ts
@@ -0,0 +1,4 @@
+export { default as TokenImage } from "./token-image";
+export { default as TokenDetails } from "./token-details";
+export { default as TokenPicker } from "./selectors/token-picker";
+export { default as EsdtTokenPicker } from "./selectors/esdt-token-picker";
--- src/screens/app/dashboard.screen/dashboard.screen.tsx
+++ src/screens/app/dashboard.screen/dashboard.screen.tsx
@@ -1,31 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import styles from "./styles.module.scss";
 
 import { Card, Icon } from "../../../components/themed";
+import { EsdtTokenPicker, TokenDetails } from "../../../components/modules/tokens";
+
+import { EsdtToken } from "../../../modules/tokens";
 import { RoutesConfig } from "../../../app/navigation";
 
 export default function DashboardScreen() {
   const navigate = useNavigate();
+  const [token, setToken] = useState<EsdtToken>();
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <Card className={styles.card} onClick={() => navigate(RoutesConfig.pingPong)}>
+        <Card>
           <div className={styles.cardContent}>
-            <h4>PING PONG</h4>
-            <div>Play a game of blockchain ping-pong</div>
+            <h4>EXPLORE TOKENS</h4>
           </div>
-          <Icon className={styles.icon} name={"sports_tennis"} />
+          <EsdtTokenPicker value={token} placeholder={"Select a token"} onChange={setToken} />
+
+          {token && <TokenDetails token={token} />}
         </Card>
-        <Card className={styles.card} onClick={() => navigate(RoutesConfig.statistics)}>
-          <div className={styles.cardContent}>
-            <h4>STATISTICS</h4>
-            <div>Play a game of blockchain ping-pong</div>
-          </div>
-          <Icon className={styles.icon} name={"insights"} />
-        </Card>
+
+        <div className={styles.sideBySideCards}>
+          <Card className={styles.card} onClick={() => navigate(RoutesConfig.pingPong)}>
+            <div className={styles.cardContent}>
+              <h4>PING PONG</h4>
+              <div>Play a game of blockchain ping-pong</div>
+            </div>
+            <Icon className={styles.icon} name={"sports_tennis"} />
+          </Card>
+          <Card className={styles.card} onClick={() => navigate(RoutesConfig.statistics)}>
+            <div className={styles.cardContent}>
+              <h4>STATISTICS</h4>
+              <div>Play a game of blockchain ping-pong</div>
+            </div>
+            <Icon className={styles.icon} name={"insights"} />
+          </Card>
+        </div>
       </div>
     </div>
   );
